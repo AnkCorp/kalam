9166179c6aa9755edd6f3b0852203ffbbba21c70
Ankit Kumar <hello@ankdev.me>
2020-04-17 01:47:42 +0530

    add file type constats

---
+++ src/kalam/constants/file_type.py
@@ -0,0 +1,22 @@
+"""All the constants related to file type."""
+
+PDF = "PDF"
+TEX = "TEX"
+TOML = "TOML"
+MD = "MARKDOWN"
+RST = "RESTRUCTURED_TEXT_FILE"
+
+# Web
+HTML = "HTML"
+CSS = "CSS"
+SCSS = "SCSS"
+SASS = "SASS"
+JS = "JS"
+JSX = "JSX"
+
+# Languages
+PY = "PYTHON"
+C = "C"
+CPP = "C++"
+
+UNKNOWN = "UNKNOWN"
