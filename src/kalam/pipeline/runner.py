f1233396b13bc5e5bc92bfe1d65860d31e5f3995
Ankur Kumar <ank@leoank.me>
2020-04-17 19:54:03 +0530

    Add dummy pipleine runner class

---
+++ src/kalam/pipeline/runner.py
@@ -0,0 +1,11 @@
+"""Runner class."""
+
+from kalam.jobs import Job
+
+
+class Runner:
+    """Creates a runner for pipleine."""
+
+    def execute(self: "Runner", job: Job) -> None:
+        """Executes the given job."""
+        pass
