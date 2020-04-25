6ef9d00c6f671ce981c4f178b8e649e471c649e4
Ankit Kumar <hello@ankdev.me>
2020-04-25 14:27:14 +0530

    generator cli update. add epilog

--- src/kalam/cli/generator.py
+++ src/kalam/cli/generator.py
@@ -6,9 +6,13 @@ from kalam.generators import PDFGenerator, TexGenerator, WebGenerator
 
 
 @click.command(
-    name="new", help="Create new project [project name is required]",
+    name="new",
+    help="Create new project.",
+    epilog="""To create new kalam project. You can use different flag\
+ like -p, --pdf for pdf documents and -t, --tex for tex document.\
+ If no flag is given then web project is created. Web project is default project.""",
 )
-@click.argument("project_name", required=True)
+@click.argument("project_name", required=True, metavar="'Name of the project'")
 @click.option("--pdf", "-p", is_flag=True, help="Create PDF document")
 @click.option("--tex", "-t", is_flag=True, help="Create TEX document")
 def create_new_project(project_name: str, pdf: bool, tex: bool) -> None:
