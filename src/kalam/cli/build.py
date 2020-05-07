afa3c2a873a3342941bebb10db69489157f0c5fc
Ankit Kumar <hello@ankdev.me>
2020-05-08 03:53:42 +0530

    add packer to bulid, for checking

--- src/kalam/cli/build.py
+++ src/kalam/cli/build.py
@@ -2,6 +2,7 @@
 
 import click
 
+from kalam.io.packer import Packer
 from kalam.jobs import Job
 from kalam.pipeline import Pipeline, Runner
 
@@ -17,3 +18,6 @@ def build() -> None:
     click.secho(f"Hahaha {pipeline}", fg="yellow")
     pipeline.run()
     click.secho(f"Did it!!!", fg="green")
+
+    packer = Packer()
+    packer.pack()
