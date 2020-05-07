91bb9cfd3f7388757c8f5954b377e4b13202f5dc
Ankit Kumar <hello@ankdev.me>
2020-05-08 03:52:31 +0530

    update file class, so that it can be used for packing

--- src/kalam/io/files/base.py
+++ src/kalam/io/files/base.py
@@ -14,14 +14,29 @@ class File:
         """Initialize file instance."""
         self.file = dict()
 
-    def set_file_info(self: "File", data: dict) -> None:
-        """Set file info."""
-        if bool(data):
-            path = data["path"]
-            self.file["abs_path"] = path
-            self.file["file_name"] = get_path_basename(path)
-            self.file["type"] = file_types.get(
-                get_file_extension_from_path(path), UNKNOWN
-            )
-            self.file["metadata"] = data["metadata"]
-            self.file["raw"] = data["raw"]
+    def set_file_info_for_packing(self: "File", data: dict) -> None:
+        """Set file info for packing."""
+        self.file["path"] = data["path"]
+        self.file["filename"] = get_path_basename(data["path"])
+        self.file["filetype"] = file_types.get(
+            get_file_extension_from_path(data["path"]), UNKNOWN
+        )
+        self.file["metadata"] = data["metadata"]
+
+    def set_file_raw_data(self: "File", data: str) -> None:
+        """Set raw data to the file."""
+        self.file["raw"] = data
+
+    def file_path(self: "File") -> str:
+        """Return the path of the file."""
+        if "path" in self.file:
+            return self.file["path"]
+        else:
+            raise KeyError("[File Error] Path is not set.")
+
+    def filetype(self: "File") -> str:
+        """Return the path of the file."""
+        if "filetype" in self.file:
+            return self.file["filetype"]
+        else:
+            raise KeyError("[File Error] File type is not set.")
