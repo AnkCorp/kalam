cca82faa2b853b0bdc620d3b4b4a36bda673a12a
Ankit Kumar <hello@ankdev.me>
2020-05-08 03:49:44 +0530

    dict utility tool add

---
+++ src/kalam/utils/dict.py
@@ -0,0 +1,22 @@
+"""Utility operations related to dictionary."""
+
+from typing import List
+
+
+def dict_all_value_in_list(d: dict) -> List[str]:
+    """Recursively reads all the value in the dictionary.
+
+    Args:
+        d: A dictionary.
+
+    Returns:
+        A list of all values.
+    """
+    all_values = []
+    for key, value in d.items():
+        if isinstance(value, dict):
+            all_values = all_values + dict_all_value_in_list(value)
+        else:
+            all_values.append(value)
+    return all_values
+
